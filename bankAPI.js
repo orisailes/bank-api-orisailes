feb44698988a6b1b4a1f7481da71061fccde9b5a
orisailes <ori1sailes@gmail.com>
2021-04-18 17:02:58 +0300

    initial commit

--- bankAPI.js
+++ bankAPI.js
@@ -6,9 +6,9 @@ const app = express();
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '../client/build')))
+    app.use(express.static(path.join(__dirname, './client/build')))
 } else {
-    app.use(express.static(path.join(__dirname, '../client/public')))
+    app.use(express.static(path.join(__dirname, './client/public')))
 }
 
 app.use(express.json());
